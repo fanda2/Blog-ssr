42d40795a7a454eb826a105c5648226a3ba48e5e
fanjt <fanjt@leofs.com.cn>
2023-08-09 16:55:54 +0800

    create ssr-app

---
+++ components/table.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    这是一个表格组件
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="less">
+  
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+   <div>
+     这是一个主页
+     <Table/>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
